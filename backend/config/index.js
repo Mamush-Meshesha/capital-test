ae166fa59405bca220c2bc6c6088083eff83ad54
Mamush-Meshesha <mamgithub@gmail.com>
2024-10-09 19:01:24 +0300

    db connection error fix1

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
-  username: process.env.DB_USERNAME,
+  username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
